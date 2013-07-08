da42f5b822b1916ccd1c6f4f594b488a2536d6b8
Michael Schanz <michael.schanz@congatec.com>
2013-07-08 09:03:38 +0200

    CGT000002 QMX6: Support for Micrel KZS9031 1GBit Phy
    initial support for Micrel KSZ9031 1GBit Ethernet Phy
    
    Signed-off-by: Michael Schanz <michael.schanz@congatec.com>

--- drivers/net/fec.c
+++ drivers/net/fec.c
@@ -1070,6 +1070,7 @@ static int fec_enet_mii_init(struct platform_device *pdev)
 	const struct platform_device_id *id_entry =
 				platform_get_device_id(fep->pdev);
 	int err = -ENXIO, i;
+	struct clk *bus_clk;
 
 	/*
 	 * The dual fec interfaces are not equivalent with enet-mac.
@@ -1098,8 +1099,10 @@ static int fec_enet_mii_init(struct platform_device *pdev)
 	/*
 	 * Set MII speed to 2.5 MHz (= clk_get_rate() / 2 * phy_speed)
 	 */
-	fep->phy_speed = DIV_ROUND_UP(clk_get_rate(fep->clk),
-					(FEC_ENET_MII_CLK << 2)) << 1;
+
+	// sml 2012-11-29: MII Speed derived from 66MHz ipg-clk
+	bus_clk = clk_get(NULL, "ipg_clk");
+	fep->phy_speed = (DIV_ROUND_UP(clk_get_rate(bus_clk),FEC_ENET_MII_CLK))-1;
 
 	/* set hold time to 2 internal clock cycle */
 	if (cpu_is_mx6q() || cpu_is_mx6dl())
